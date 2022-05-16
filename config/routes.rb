fc533977312b572b9468bb027c3f88434c0f367f
laemdfk <kappa021618@icloud.com>
2022-05-16 07:05:25 +0000

    customer#controller実装

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
 namespace :admin do
   root to: "orders#index"
+  resources:customers, only: [:index, :show, :edit, :update]
 end
 
 root to: "public/items#top"
