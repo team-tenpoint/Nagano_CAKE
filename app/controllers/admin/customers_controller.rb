4091b337c988283e3df03df4948a13f2c5cb1c28
laemdfk <kappa021618@icloud.com>
2022-05-17 05:15:46 +0000

    Add index,edit,show view追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,16 +15,16 @@ class Admin::CustomersController < ApplicationController
   def update
    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to admin_customer_path
-    # else
-    #   render "edit"
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render "edit"
     end
   end
 
   private
 
    def customer_params
-     params.require(:coustmer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_cord, :address, :telephone_number, :password, :is_deleted)
+     params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_cord, :address, :telephone_number, :password, :is_deleted)
    end
 
 
