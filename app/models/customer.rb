e4fd1149da1941c5e504ad6cbfb3e7e04e46d7cb
laemdfk <kappa021618@icloud.com>
2022-05-18 06:30:33 +0000

    [Add]viewページ　リンク設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # アソシエーション設定 注文、アドレス、カート(内の商品)
+
+  # has_many :orders, dependent: :destroy
+
+  # has_many :address, dependent: :destroy
+
+  # has_many :cart_items, dependent: :destroy
+
 end
